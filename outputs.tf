81a90ff1fda2899a022f09170018337042ffe98e
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:34 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,17 @@
-output "spring_cloud_build_pack_bindings" {
-  description = "All spring_cloud_build_pack_binding resources"
-  value       = azurerm_spring_cloud_build_pack_binding.spring_cloud_build_pack_bindings
-}
 output "spring_cloud_build_pack_bindings_binding_type" {
-  description = "List of binding_type values across all spring_cloud_build_pack_bindings"
-  value       = [for k, v in azurerm_spring_cloud_build_pack_binding.spring_cloud_build_pack_bindings : v.binding_type]
+  description = "Map of binding_type values across all spring_cloud_build_pack_bindings, keyed the same as var.spring_cloud_build_pack_bindings"
+  value       = { for k, v in azurerm_spring_cloud_build_pack_binding.spring_cloud_build_pack_bindings : k => v.binding_type }
 }
 output "spring_cloud_build_pack_bindings_launch" {
-  description = "List of launch values across all spring_cloud_build_pack_bindings"
-  value       = [for k, v in azurerm_spring_cloud_build_pack_binding.spring_cloud_build_pack_bindings : v.launch]
+  description = "Map of launch values across all spring_cloud_build_pack_bindings, keyed the same as var.spring_cloud_build_pack_bindings"
+  value       = { for k, v in azurerm_spring_cloud_build_pack_binding.spring_cloud_build_pack_bindings : k => v.launch }
 }
 output "spring_cloud_build_pack_bindings_name" {
-  description = "List of name values across all spring_cloud_build_pack_bindings"
-  value       = [for k, v in azurerm_spring_cloud_build_pack_binding.spring_cloud_build_pack_bindings : v.name]
+  description = "Map of name values across all spring_cloud_build_pack_bindings, keyed the same as var.spring_cloud_build_pack_bindings"
+  value       = { for k, v in azurerm_spring_cloud_build_pack_binding.spring_cloud_build_pack_bindings : k => v.name }
 }
 output "spring_cloud_build_pack_bindings_spring_cloud_builder_id" {
-  description = "List of spring_cloud_builder_id values across all spring_cloud_build_pack_bindings"
-  value       = [for k, v in azurerm_spring_cloud_build_pack_binding.spring_cloud_build_pack_bindings : v.spring_cloud_builder_id]
+  description = "Map of spring_cloud_builder_id values across all spring_cloud_build_pack_bindings, keyed the same as var.spring_cloud_build_pack_bindings"
+  value       = { for k, v in azurerm_spring_cloud_build_pack_binding.spring_cloud_build_pack_bindings : k => v.spring_cloud_builder_id }
 }
 
